a5f98c7a80d4aed28b76698ffa2a591d49b49098
sreng sun <rocky.sun@pathmazing.com>
2023-09-11 14:04:12 +0700

    ok

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -4,9 +4,6 @@ import "../styles/tailwind.css"
 
 import { withThemeByClassName } from "@storybook/addon-styling"
 
-/* TODO: update import to your tailwind styles file. If you're using Angular, inject this through your angular.json config instead */
-// import "../src/index.css"
-
 const preview: Preview = {
   parameters: {
     actions: { argTypesRegex: "^on[A-Z].*" },
